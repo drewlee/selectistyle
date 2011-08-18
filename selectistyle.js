ad551e62418990dc964c0877ccb876a96a0c422c
Andrew Lee <starsthatfell@gmail.com>
2011-08-18 04:24:31 -0500

    Edited selectistyle.js via GitHub

--- selectistyle.js
+++ selectistyle.js
@@ -1,7 +1,7 @@
 (function($){
-
 $.fn.selectistyle = function(opts){
-    var st = {
+    var $doc = $(document),
+    st = {
         prefix: 'selectistyle',
         onEffect: 'fadeIn',
         offEffect: 'fadeOut',
@@ -10,141 +10,137 @@ $.fn.selectistyle = function(opts){
         callback: null
     };
     
-    var _methods = {
-        init: function(elem){
-            this.$sel = $(elem);
-            this.$doc = $(document);
-            
-            var wdth = this.$sel.outerWidth() + st.widthAdj;
-            var ids = this.generateIds();
-            
-            elem.style.display = 'none';
-            
-            this.insertSrc(this.generateHtml(ids), wdth, ids.cId);
-            this.bindEvents(ids.aId, ids.divId);
-        },
+    function Selectistyle($sel){
+        var cls = 'ssd-active',
+            _this = this;
         
-        generateIds: function(){
+        this.$sel = $sel;
+        
+        this.generateIds = function(){
             var id = this.$sel[0].id || 'select_' + Date.parse(new Date()),
                 cls = st.prefix + '_',
                 ids = {
+                    sId: id,
                     cId: cls + 'container_' + id,
                     aId: cls+ 'a_' + id,
                     divId: cls + 'div_' + id,
                     ulId: cls + 'ul_' + id
                 };
 
-            return ids;
-        },
+            this.ids = ids;
+        };
         
-        generateHtml: function(ids){
+        this.generateHtml = function(){
             var elem = this.$sel[0],
+                ids = this.ids,
                 cls = st.prefix + '-',
                 sIdx = elem.selectedIndex,
                 opts = elem.options;
             
             var html = '<a id="' + ids.aId + '" class="' + cls + 'hotspot">\n'
-                    + '<span class="' + cls + 'arrow"></span>\n'
-                    + '<span class="' + cls + 'text">' + opts[sIdx].text + '</span>\n'
+                + '<span class="' + cls + 'arrow"></span>\n'
+                + '<span class="' + cls + 'text">' + opts[sIdx].text + '</span>\n'
                 + '</a>\n'
                 + '<div id="' + ids.divId + '" class="' + cls + 'div" style="display:none;">\n'
-                    + '<ul id="' + ids.ulId + '" class="' + cls + 'ul">\n';
+                + '<ul id="' + ids.ulId + '" class="' + cls + 'ul">\n';
             
             for(var i=0; opts[i]; i++){
                 html += '<li class="' + cls + 'li' + (i === sIdx ? ' ssa-active' : '') + '">'
-                        + '<a class="' + cls + 'a">' + opts[i].text + '</a>'
+                    + '<a class="' + cls + 'a">' + opts[i].text + '</a>'
                     + '</li>\n';
             }
             
             html += '</ul>\n</div>\n';
             
-            return html;
-        },
+            this.html = html;
+        };
         
-        insertSrc: function(html, wdth, cId){
-            this.$sel.wrap('<div id="' + cId + '" class="' + st.prefix + '-container" style="width:' + wdth + 'px;" />')
+        this.insertSrc = function(){
+            var $sel = this.$sel,
+                wdth = $sel.outerWidth() + st.widthAdj;
+            
+            $sel.hide()
+                .wrap('<div id="' + this.ids.cId + '" class="' + st.prefix + '-container" style="width:' + wdth + 'px;" />')
                 .parent()
-                .append(html);
-        },
+                .append(this.html);
+        };
         
-        bindEvents: function(aId, divId){
-            this.$dd = $('#' + divId);
-            this.$a = $('#' + aId);
-            
-            this.$a.bind('click.' + 'selectistyle', this.evts.handleClick);
-            this.$dd.bind('click.' + 'selectistyle', this.evts.handleDropdown);
-        },
+        this.hideDropDown = function(){
+            _this.$dd[st.offEffect](st.speed).removeClass(cls);
+            $doc.unbind('click', _this.handleDocClick);
+        };
         
-        evts: {
-            cls: 'ssd-active',
+        this.handleDocClick = function(){
+            _this.hideDropDown();
+        };
+        
+        this.handleClick = function(e){
+            var $this = $(this),
+                $dd = _this.$dd;
+
+            if($dd.hasClass(cls)){
+                _this.hideDropDown();
+            }else{
+                $dd[st.onEffect](st.speed).addClass(cls);
+                $doc.bind('click', _this.handleDocClick);
+            }
             
-            hideDropDown: function(){
-                var _this = _methods;
-                
-                _this.$dd[st.offEffect](st.speed).removeClass(_this.evts.cls);
-                _this.$doc.unbind('click', _this.evts.handleDocClick);
-            },
+            e.stopPropagation();
+        };
+        
+        this.handleDropdown = function(e){
+            var $tget = $(e.target),
+                $sel = _this.$sel,
+                $a = _this.$a,
+                $li, txt, idx, cls = 'ssa-active';
             
-            handleDocClick: function(){
-                _methods.evts.hideDropDown();
-            },
-            
-            handleClick: function(e){
-                var $this = $(this),
-                    _this = _methods,
-                    $dd = _this.$dd;
+            if($tget.closest('a').length){
+                $tget = $tget.closest('a');
+                $li = $tget.parent();
                 
-                if($dd.hasClass(_this.evts.cls)){
-                    _this.evts.hideDropDown();
-                }else{
-                    $dd[st.onEffect](st.speed).addClass(_this.evts.cls);
-                    _this.$doc.click(_this.evts.handleDocClick);
-                }
+                if($li.hasClass(cls)){return;}
                 
-                e.stopPropagation();
-            },
-            
-            handleDropdown: function(e){
-                var _this = _methods,
-                    $tget = $(e.target),
-                    sel = _this.$sel[0],
-                    $li, txt, idx, cls = 'ssa-active';
+                $li.siblings('li.' + cls).removeClass(cls);
+                $li.addClass(cls);
                 
-                if($tget.closest('a').length){
-                    $tget = $tget.closest('a');
-                    $li = $tget.parent();
-                    
-                    if($li.hasClass(cls)){return;}
-                    
-                    $li.siblings('li.' + cls).removeClass(cls);
-                    $li.addClass(cls);
-                    
-                    idx = $li.index();
-                    txt = $tget.text();
-                    
-                    _this.$a.find('span.' + st.prefix + '-text').text(txt);
-                    sel.selectedIndex = idx;
-                    
-                    _this.$sel.change();
-                    
-                    if(st.callback){
-                        st.callback.call(sel, sel.value, txt);
-                    }
+                idx = $li.index();
+                txt = $tget.text();
+                
+                $a.find('span.' + st.prefix + '-text').text(txt);
+                $sel[0].selectedIndex = idx;
+                
+                $sel.change();
+                
+                if(st.callback){
+                    st.callback.call($sel[0], $sel[0].value, txt);
                 }
             }
-        }
-    };
+        };
+        
+        this.bindEvents = function(){
+            var type = 'click.selectistyle';
+            
+            this.$a = $('#' + this.ids.aId);
+            this.$dd = $('#' + this.ids.divId);
+
+            this.$a.bind(type, this.handleClick);
+            this.$dd.bind(type, this.handleDropdown);
+        };
+        
+        this.generateIds();
+        this.generateHtml();
+        this.insertSrc();
+        this.bindEvents();
+    }
     
     $.extend(st, opts || {});
     
     return this.each(function(){
         var $this = $(this);
         
-        if(!$this.is('select')){return;}
-
-        _methods.init(this);
+        if($this.is('select')){
+            new Selectistyle($this);
+        }
     });
-    
 };
-
 })(jQuery);
